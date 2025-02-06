a73115cb89a98fd77d710bf9744be733135bccd1
Sam Wale-Bogunjoko <feranwb@gmail.com>
2025-02-06 17:54:31 -0500

    Initial commit for Canvas Reminder Extension

---
+++ background.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Procrastination Helper extension installed.");
+});
---
+++ popup.js
@@ -0,0 +1,11 @@
+document.getElementById("rem-butt").addEventListener("click",()=> {
+    console.log("Reminder button clicked");
+    //Notification for test
+    chrome.notifications.create({
+        type: "basic",
+        iconUrl: "icon.png",
+        title: "Reminder",
+        message: "Time to do your task"
+    })
+    console.log("Reminder created");
+});
